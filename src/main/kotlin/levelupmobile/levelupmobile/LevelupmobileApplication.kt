6fd18c9b9c77b480cdc945e423a243eab4baa22a
Matias1402 <matias18torres18@gmail.com>
2025-11-25 23:30:21 -0300

    Iniciado proyecto en spring para backend

---
+++ src/main/kotlin/levelupmobile/levelupmobile/LevelupmobileApplication.kt
@@ -0,0 +1,11 @@
+package levelupmobile.levelupmobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LevelupmobileApplication
+
+fun main(args: Array<String>) {
+	runApplication<LevelupmobileApplication>(*args)
+}
---
+++ src/test/kotlin/levelupmobile/levelupmobile/LevelupmobileApplicationTests.kt
@@ -0,0 +1,13 @@
+package levelupmobile.levelupmobile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LevelupmobileApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
